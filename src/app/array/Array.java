f85839d210e8e2aa47f85d7f5f62a49370bc735a
timofey <timofey_email@example.com>
2018-04-16 19:09:47 +0300

    - improved Array class implementation

--- src/app/array/Array.java
+++ src/app/array/Array.java
@@ -1,8 +1,54 @@
 package app.array;
 
 
-public class Array {
+public class Array<T> {
 
+    private final T[] array;
 
+    private int size;
+
+    public Array(final int num) {
+        array = (T[]) new Object[num];
+        size = 0;
+    }
+
+    public boolean find(final T element) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == element) return true;
+        }
+
+        return false;
+    }
+
+    public void insert(final T element) {
+        array[size] = element;
+        size++;
+    }
+
+    public boolean remove(final T element) {
+        int i;
+
+        for (i = 0; i < size; i++) {
+            if (array[i] == element) break;
+        }
+
+        if (i == size) return false;
+        else {
+            for (int k = i; k < size - 1; k++) {
+                array[k] = array[k + 1];
+            }
+
+            size--;
+
+            return true;
+        }
+    }
+
+    public void display() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+            System.out.println();
+        }
+    }
 
 }
--- src/app/array/Main.java
+++ src/app/array/Main.java
@@ -3,6 +3,26 @@ package app.array;
 
 public class Main {
 
+    public static void main(String[] args) {
+        final Array<Integer> array = new Array<>(6);
 
+        array.insert(2);
+        array.insert(15);
+        array.insert(23);
+        array.insert(34);
+        array.insert(100);
+        array.insert(10);
+
+        array.display();
+
+        if (array.find(34)) System.out.println("Found");
+        else System.out.println("Not found");
+
+        array.remove(23);
+        array.remove(2);
+        array.remove(10);
+
+        array.display();
+    }
 
 }
