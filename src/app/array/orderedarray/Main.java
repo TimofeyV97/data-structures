0eabf7c9396f701b2c23bbd78ac5ed64d3c4a288
timofey <timofey_email@example.com>
2018-04-18 00:32:10 +0300

    - Ordered Array class implemented

---
+++ src/app/array/orderedarray/Main.java
@@ -0,0 +1,32 @@
+package app.array.orderedarray;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        final OrderedArray orderedArray = new OrderedArray(100);
+
+        orderedArray.insert(23);
+        orderedArray.insert(12);
+        orderedArray.insert(98);
+        orderedArray.insert(1200);
+        orderedArray.insert(567);
+        orderedArray.insert(100);
+        orderedArray.insert(51);
+
+        orderedArray.display();
+
+        if (orderedArray.find(12) != orderedArray.getSize()) System.out.println("Found");
+        else System.out.println("Not found");
+
+        orderedArray.remove(100);
+        orderedArray.remove(1200);
+        orderedArray.remove(98);
+
+        if (orderedArray.find(100) != orderedArray.getSize()) System.out.println("Found");
+        else System.out.println("Not found");
+
+        orderedArray.display();
+    }
+
+}
---
+++ src/app/array/orderedarray/OrderedArray.java
@@ -0,0 +1,66 @@
+package app.array.orderedarray;
+
+
+public class OrderedArray {
+
+    private long[] array;
+
+    private int size;
+
+    public OrderedArray(final int num) {
+        array = new long[num];
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int find(final long element) {
+        int lowerBound = 0;
+        int upperBound = size - 1;
+        int current;
+
+        while (true) {
+            current = (lowerBound + upperBound) / 2;
+
+            if (array[current] == element)  return current;
+            else if (lowerBound > upperBound) return size;
+            else {
+                if (array[current] < element) lowerBound = current + 1;
+                else upperBound = current - 1;
+            }
+        }
+    }
+
+    public void insert(final long element) {
+        int i;
+
+        for (i = 0; i < size; i++) {
+            if (array[i] > element) break;
+        }
+
+        for (int k = size; k > i; k--) array[k] = array[k - 1];
+
+        array[i] = element;
+        size++;
+    }
+
+    public boolean remove(final long element) {
+        int i = find(element);
+
+        if (i == size) return false;
+        else {
+            for (int k = i; k < size; k++) array[k] = array[k + 1];
+            size--;
+        }
+
+        return true;
+    }
+
+    public void display() {
+        for (int i = 0; i < size; i++) System.out.print(array[i] + " ");
+        System.out.println();
+    }
+
+}
