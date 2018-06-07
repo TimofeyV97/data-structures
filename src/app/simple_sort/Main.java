f98d27442cec92adcdd7bb2d217411fd23b6516d
timofey <timofey_email@example.com>
2018-06-07 18:56:17 +0300

    - Class BubbleSort implemented

--- src/app/simple_sort/Main.java
+++ src/app/simple_sort/Main.java
@@ -4,8 +4,10 @@ package app.simple_sort;
 public class Main {
 
     public static void main(String[] args) {
-        SimpleSort bs = new BubbleSort(10);
+        BubbleSort bs = new BubbleSort(10);
+        SelectionSort ss = new SelectionSort(10);
 
+        System.out.println("Bubble sort:");
         bs.insert(10);
         bs.insert(20);
         bs.insert(5);
@@ -16,12 +18,24 @@ public class Main {
         bs.insert(40);
         bs.insert(2);
         bs.insert(100);
-
         bs.display();
-
         bs.sort();
-
         bs.display();
+
+        System.out.println("\nSelection sort:");
+        ss.insert(10);
+        ss.insert(20);
+        ss.insert(5);
+        ss.insert(77);
+        ss.insert(50);
+        ss.insert(110);
+        ss.insert(81);
+        ss.insert(40);
+        ss.insert(2);
+        ss.insert(100);
+        ss.display();
+        ss.sort();
+        ss.display();
     }
 
 }
---
+++ src/app/simple_sort/SelectionSort.java
@@ -0,0 +1,25 @@
+package app.simple_sort;
+
+
+public class SelectionSort extends SimpleSort {
+
+    public SelectionSort(final int num) {
+        super(num);
+    }
+
+    @Override
+    public void sort() {
+        int min;
+
+        for (int i = 0; i < size - 1; i++) {
+            min = i;
+
+            for (int j = i + 1; j < size; j++) {
+                if (array[j] < array[min]) min = j;
+            }
+
+            swap(i, min);
+        }
+    }
+
+}
